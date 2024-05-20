c976dc18ad68784077a70f9e0fff7044a2594f47
JacopoFerrara <fer.j936@gmail.com>
2024-05-20 20:08:03 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'FoodLite',
+  webDir: 'dist'
+};
+
+export default config;
